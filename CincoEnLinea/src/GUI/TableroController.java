90ed78e226c8128e38e7c22f0e6dd9cc6e0d7105
Esmeralda14 <meme.f14@gmail.com>
2017-09-25 20:55:03 -0500

    Modificaciones
    Creacion de la interfaz del tablero y palabras agregadas a la internacionalizacion

---
+++ CincoEnLinea/src/GUI/TableroController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Esmeralda
+ */
+public class TableroController implements Initializable {
+
+    @FXML
+    private Label labelJugador;
+    @FXML
+    private TextField textJugador;
+    @FXML
+    private Button botonReiniciar;
+    @FXML
+    private Button botonMenu;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    ResourceBundle resources = ResourceBundle.getBundle("resources.idioma");    
+    
+    public void configurarIdioma(){
+        labelJugador.setText(resources.getString("labelJugador"));
+        botonReiniciar.setText(resources.getString("botonReiniciar"));
+        botonMenu.setText(resources.getString("botonMenu"));
+
+    }
+    
+    @FXML
+    public void cambiarIdiomaUS(){
+        resources = ResourceBundle.getBundle("resources.idioma_en_US");
+        configurarIdioma();
+    }
+    
+    @FXML
+    public void cambiarIdiomaMX(){
+        resources = ResourceBundle.getBundle("resources.idioma_es_MX");
+        configurarIdioma();
+        
+    }
+}
